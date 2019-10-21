6bb031bf550455aa98fcfebf1fb4638cc50ccc79
chennatech <56807712+chennatech@users.noreply.github.com>
2019-10-21 18:19:36 +0530

    hi

---
+++ MyCode/src/com/telisko/test/Alians.java
@@ -0,0 +1,5 @@
+package com.telisko.test;
+
+public class Alians {
+
+}
---
+++ MyCode/src/com/telisko/test/Calculator.java
@@ -0,0 +1,11 @@
+package com.telisko.test;
+
+public class Calculator {
+	public int add(int i, int j) {
+		return i + j;
+	}
+
+	public int sub(int i, int j) {
+		return i - j;
+	}
+}

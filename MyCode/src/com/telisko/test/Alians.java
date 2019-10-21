eebd38c2ea1a7b23f5ecd00635e0a4fca9605a78
chennatech <56807712+chennatech@users.noreply.github.com>
2019-10-21 18:41:00 +0530

    sysout

--- MyCode/src/com/telisko/test/Alians.java
+++ MyCode/src/com/telisko/test/Alians.java
@@ -3,7 +3,7 @@ package com.telisko.test;
 public class Alians {
 	public static void main(String[] args) {
 		Calculator cl = new Calculator();
-		
+		System.out.println("hello");
 		System.out.println(cl.add(10, 20));
 	}
 }
